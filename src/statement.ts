175710ff79c5b0c4efd8879c22aaf7b0424c72ad
Asif Momin <asifmomin@sahaj.ai>
2022-09-20 10:09:23 +1000

    refactor :extractFunction for calculate the amount for performance

--- src/statement.ts
+++ src/statement.ts
@@ -1,4 +1,26 @@
-import {Invoice, Plays} from "./types";
+import {Invoice, Performance, Play, Plays} from "./types";
+
+function amountFor(play: Play, perf: Performance) {
+    let result = 0;
+    switch (play.type) {
+        case "tragedy":
+            result = 40000;
+            if (perf.audience > 30) {
+                result += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            result = 30000;
+            if (perf.audience > 20) {
+                result += 10000 + 500 * (perf.audience - 20);
+            }
+            result += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+    return result;
+}
 
 export function statement (invoice:Invoice, plays:Plays) {
     let totalAmount = 0;
@@ -10,24 +32,7 @@ export function statement (invoice:Invoice, plays:Plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = amountFor(play, perf);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
